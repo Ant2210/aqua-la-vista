599cd6246a8908e848e3c58509be5e3d5dc61b27
Ant <120403717+Ant2210@users.noreply.github.com>
2023-07-14 21:11:34 +0100

    Restyle navbar and add logo (#7)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,50 +2,51 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import "../index.css";
-import logo from "../logo.svg";
-import {FaHouseChimney} from "react-icons/fa6";
-import {FaCircleQuestion} from "react-icons/fa6";
-import {FaRegNewspaper} from "react-icons/fa6";
-import {FaEnvelope} from "react-icons/fa6";
+import logo from "../assets/imgs/logo-transparent.png";
+import { FaHouseChimney, FaCircleQuestion, FaRegNewspaper, FaEnvelope } from "react-icons/fa6";
 
 const PageNav = () => {
     return (
         <Navbar expand="md" id="nav-styling">
             <Container>
                 <Navbar.Brand href="#home">
-                    <img src={logo} className="App-logo" alt="logo" />
+                    <img src={logo} id="nav-logo" alt="logo" />
                 </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Toggle aria-controls="basic-navbar-nav" id="navbar-toggler" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ms-auto">
                         <Nav.Link
                             href="#home"
-                            className="hvr-underline-from-center .nav-link-styling">
-                            <span className="pe-2">
+                            id="nav-link"
+                            className="hvr-underline-from-center">
+                            <span className="purple pe-2">
                                 <FaHouseChimney />
                             </span>
                             Home
                         </Nav.Link>
                         <Nav.Link
                             href="#link"
-                            className="hvr-underline-from-center .nav-link-styling">
-                            <span className="pe-2">
+                            id="nav-link"
+                            className="hvr-underline-from-center">
+                            <span className="purple pe-2">
                                 <FaCircleQuestion />
                             </span>
                             About
                         </Nav.Link>
                         <Nav.Link
                             href="#link"
-                            className="hvr-underline-from-center .nav-link-styling">
-                            <span className="pe-2">
+                            id="nav-link"
+                            className="hvr-underline-from-center">
+                            <span className="purple pe-2">
                                 <FaRegNewspaper />
                             </span>
                             News
                         </Nav.Link>
                         <Nav.Link
                             href="#link"
+                            id="nav-link"
                             className="hvr-underline-from-center .nav-link-styling">
-                            <span className="pe-2">
+                            <span className="purple pe-2">
                                 <FaEnvelope />
                             </span>
                             Contact
