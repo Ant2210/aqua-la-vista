69e3233d523480e2d7ec8533a7172d41007434fd
Aiga <aiga.andrejeva@gmail.com>
2023-07-15 03:07:29 +0000

    Hero mobile responsive

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Button from "react-bootstrap/Button";
 import "./index.css";
 import PageNav from "./components/Navbar";
 import Footer from "./components/Footer";
+import HeroImage from "./components/HeroImage";
 
 function App() {
     return (
         <>
             <PageNav />
+            <HeroImage />
             <Footer />
         </>
     );
