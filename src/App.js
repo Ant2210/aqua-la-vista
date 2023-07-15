beed958ae4c60be3e54dd5ad09c7f7d73d3cdc25
Ant <120403717+Ant2210@users.noreply.github.com>
2023-07-15 02:03:12 +0100

    Ant (#12)
    * Restyle navbar and add logo
    
    * Switch altered logo
    
    * Fix merge conflict
    
    * Fix missing { in index.css
    
    * Create footer

--- src/App.js
+++ src/App.js
@@ -2,18 +2,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Button from "react-bootstrap/Button";
 import "./index.css";
 import PageNav from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
     return (
         <>
             <PageNav />
-            <div className="container text-center">              
-              <Button variant="primary">Hello</Button>{" "}
-              <Button variant="secondary" className="my-2">
-                  From{" "}
-              </Button>{" "}
-              <Button variant="success">Bootstrap</Button>{" "}
-            </div>
+            <Footer />
         </>
     );
 }
