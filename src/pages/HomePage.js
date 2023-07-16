47557e445814dd4fc9c272e0fe54b183cc0f3e8f
Ant <ant.griffiths22@gmail.com>
2023-07-16 15:06:57 +0100

    Add chat to homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import HeroImage from '../components/HeroImage'
 import { Navbar } from 'react-bootstrap'
+import Chat from '../components/Chat'
 
 export default function HomePage() {
   return (
     <div>
-        <p>homepage</p>
+      <Chat />
     </div>
   )
 }
