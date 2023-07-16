bb154934c3e7d9bc8a2695784cac7584ca239c56
Ant <ant.griffiths22@gmail.com>
2023-07-16 21:11:51 +0100

    Homepage style update and code refactor

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,13 +3,15 @@ import HeroImage from '../components/HeroImage'
 import { Navbar } from 'react-bootstrap'
 import Chat from '../components/Chat'
 import CarouselCountries from '../components/CarouselCountries'
+import Quote from '../components/Quote'
 
 export default function HomePage() {
   return (
-    <div>
+    <>
       <HeroImage />
+      <Quote />
       <Chat />
       <CarouselCountries />
-    </div>
+    </>
   )
 }
