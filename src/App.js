5261e4be71455a0bdcc9e2d84e73efebb156d812
Aiga <aiga.andrejeva@gmail.com>
2023-07-16 01:14:10 +0000

    Hero image change

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ function App() {
         <>
             <PageNav />
             <HeroImage />
-            <CarouselCountries />
             <EventsList />
+            <CarouselCountries />
             <Footer />
         </>
     );
