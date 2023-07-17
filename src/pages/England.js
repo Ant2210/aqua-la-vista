19a467c18d4ded7e719090131aa825a70b876c01
jerrybonq <jerrybonq@gmail.com>
2023-07-17 11:30:18 +0100

    style countries descriptions and add images.

--- src/pages/England.js
+++ src/pages/England.js
@@ -1,36 +1,98 @@
 import React from 'react';
 import '../index.css';
+import img1 from '../assets/imgs/gaddings-dam-england.webp'
+import img2 from '../assets/imgs/hampstead-ponds.jpg'
+import img3 from '../assets/imgs/cumbria-lake-district.jpeg'
+import img4 from '../assets/imgs/lulworth-cove.jpg'
+import img5 from '../assets/imgs/river-dart.jpg'
+import img6 from '../assets/imgs/river-wye.jpg'
 
 
+    const England = () => {
+        return (
+          <div>
+            <div className="article-page-intro"> 
+                <h2 className="page-title">Welcome To England</h2>
+                <p>
+                In England, the "right to roam" allows public access to certain land, including rivers, lakes, and coastal areas. However, this does not grant unrestricted access to all private land.
+                </p>
+                <p>Respect signs and boundaries, seek permission when necessary, and follow any local regulations or guidance.</p>
+            </div>
 
-const England = () => {
-  return (
-    <>
-    <h1 className='country-title'>Welcome To England</h1>
-      <p className='country-desc'>In England, the "right to roam" allows public access to certain land, including rivers, lakes, and coastal areas. However, this does not grant unrestricted access to all private land.
-      </p>
-      <p className='country-desc'>Respect signs and boundaries, seek permission when necessary, and follow any local regulations or guidance.
-      </p>
-      <h2 className='country-slogan'>6 beautiful Wild Swimming spots in England!</h2>
-      <h3 className='country-place'>1. Gaddings Dam, West Yorkshire</h3>
-      <p className='country-desc'>Known unofficially as the highest beach in the country, Gaddings Dam is located above the town of Todmorden, which sits on the boundary between Yorkshire and Lancashire. It has a real (small) sand beach and is not too far from the Pennine Way. You have to walk to get there and be prepared to share the space with cows.
-      </p>
-      <h3 className='country-place'>2. Hampstead Ponds, London</h3>
-      <p className='country-desc'>Located within Hampstead Heath, the Hampstead Ponds offer a unique wild swimming experience in the heart of London. The three freshwater swimming ponds—Hampstead Mixed Pond, Hampstead Men's Pond, and Hampstead Ladies' Pond—are popular among locals.
-      </p>
-      <h3 className='country-place'>3. Lake District, Cumbria</h3>
-      <p className='country-desc'>With its stunning lakes and picturesque landscapes, the Lake District offers numerous opportunities for wild swimming. Places like Lake Windermere, Derwentwater, Buttermere, and Ullswater are popular choices for swimmers seeking tranquility and natural beauty.
-      </p>
-      <h3 className='country-place'>4. Lulworth Cove, Dorset</h3>
-      <p className='country-desc'>Lulworth Cove is a beautiful natural harbor along the Jurassic Coast in Dorset. The sheltered cove features clear turquoise waters, making it a popular spot for wild swimmers looking to take a dip in the sea while surrounded by breathtaking cliffs.
-      </p>
-      <h3 className='country-place'>5. River Dart, Dartmoor</h3>
-      <p className='country-desc'>The River Dart in Dartmoor National Park provides idyllic settings for wild swimming. The river features deep pools, waterfalls, and clear water. The area around Dartmeet is particularly renowned for swimming.
-      </p>
-      <h3 className='country-place'>6. River Wye, Herefordshire</h3>
-      <p className='country-desc'>The River Wye, which flows through the stunning Wye Valley, offers excellent wild swimming opportunities. The river boasts crystal-clear waters, sandy beaches, and peaceful stretches perfect for a refreshing swim.
-      </p>
-    </>
+            <div className="article-container">
+                <div className="article-img">
+                    <img src={img1} alt="People swimming in the open water"/>
+                </div>
+                <div className="article-text">
+                    <h4>Gaddings Dam, West Yorkshire</h4>
+                    <p>
+                    Known unofficially as the highest beach in the country, Gaddings Dam is located above the town of Todmorden, which sits on the boundary between Yorkshire and Lancashire. It has a real (small) sand beach and is not too far from the Pennine Way. You have to walk to get there and be prepared to share the space with cows.
+                    </p>
+                </div>
+            </div>
+
+            <div className="article-container article-container-reverse">
+                <div className="article-img">
+                    <img src={img2} alt="People swimming in the open water"/>
+                </div>
+                <div className="article-text">
+                    <h4>Hampstead Ponds, London</h4>
+                    <p>
+                    Located within Hampstead Heath, the Hampstead Ponds offer a unique wild swimming experience in the heart of London. The three freshwater swimming ponds—Hampstead Mixed Pond, Hampstead Men's Pond, and Hampstead Ladies' Pond—are popular among locals.  
+                    </p>
+                </div>
+            </div>
+
+            <div className="article-container">
+                <div className="article-img">
+                    <img src={img3} alt="People swimming in the open water"/>
+                </div>
+                <div className="article-text">
+                    <h4>Lake District, Cumbria</h4>
+                    <p>
+                    With its stunning lakes and picturesque landscapes, the Lake District offers numerous opportunities for wild swimming. Places like Lake Windermere, Derwentwater, Buttermere, and Ullswater are popular choices for swimmers seeking tranquility and natural beauty.
+                    </p>
+                </div>
+            </div>
+
+            <div className="article-container article-container-reverse">
+                <div className="article-img">
+                    <img src={img4} alt="People swimming in the open water"/>
+                </div>
+                <div className="article-text">
+                    <h4>Lulworth Cove, Dorset</h4>
+                    <p>
+                    Lulworth Cove is a beautiful natural harbor along the Jurassic Coast in Dorset. The sheltered cove features clear turquoise waters, making it a popular spot for wild swimmers looking to take a dip in the sea while surrounded by breathtaking cliffs.
+                    </p>
+                </div>
+            </div>
+
+            <div className="article-container">
+                <div className="article-img">
+                    <img src={img5} alt="People swimming in the open water"/>
+                </div>
+                <div className="article-text">
+                    <h4>River Dart, Dartmoor</h4>
+                    <p>
+                    The River Dart in Dartmoor National Park provides idyllic settings for wild swimming. The river features deep pools, waterfalls, and clear water. The area around Dartmeet is particularly renowned for swimming.
+                    </p>
+                </div>
+            </div>
+
+            <div className="article-container article-container-reverse">
+                <div className="article-img">
+                    <img src={img6} alt="People swimming in the open water"/>
+                </div>
+                <div className="article-text">
+                    <h4>River Wye, Herefordshire</h4>
+                    <p>
+                    The River Wye, which flows through the stunning Wye Valley, offers excellent wild swimming opportunities. The river boasts crystal-clear waters, sandy beaches, and peaceful stretches perfect for a refreshing swim.
+                    </p>
+                </div>
+            </div>
+          </div>
+
+
 
   );
 };
--- src/pages/Ireland.js
+++ src/pages/Ireland.js
@@ -1,40 +1,97 @@
 import React from 'react';
 import '../index.css';
+import img1 from '../assets/imgs/ireland.jpg'
+import img2 from '../assets/imgs/inch-beach.jpg'
+import img3 from '../assets/imgs/lough-gill.jpg'
+import img4 from '../assets/imgs/lough-hyne.jpg'
+import img5 from '../assets/imgs/Salthill-Galway.jpg'
+import img6 from '../assets/imgs/silver-strand-malin-beg-DSC0028.jpg'
 
 
-
-const Ireland = () => {
-  return (
-    <>
-    <h1 className='country-title'>Welcome To Ireland</h1>
-      <p className='country-desc'>In Ireland, there is a tradition of access to the countryside and natural water bodies. As long as you respect the environment, local regulations, and private property rights, you can usually enjoy wild swimming in rivers, lakes, and coastal areas.
+    const Ireland = () => {
+        return (
+          <div>
+            <div className="article-page-intro"> 
+                <h2 className="page-title">Welcome To Ireland</h2>
+                <p>
+                In Ireland, there is a tradition of access to the countryside and natural water bodies. As long as you respect the environment, local regulations, and private property rights, you can usually enjoy wild swimming in rivers, lakes, and coastal areas.
       When engaging in wild swimming, it's important to consider safety, water quality, and any local regulations that may be in place. Additionally, always respect the environment and other users of the area - especially local residents -  to ensure a positive and sustainable experience for everyone.
-      </p>
-      <p className='country-desc'>Be mindful of any local regulations or restrictions that may apply to specific areas. Some water bodies or beaches may have specific bylaws, designated swimming areas, or seasonal restrictions. Check with local authorities or signs for any specific rules.
-      </p>
-      <h2 className='country-slogan'>7 beautiful Wild Swimming spots in Ireland!</h2>
-      <h3 className='country-place'>1. Glendalough Upper Lake, County Wicklow</h3>
-      <p className='country-desc'>Nestled within the picturesque Glendalough Valley, the Upper Lake in County Wicklow offers an enchanting setting for wild swimming. Surrounded by stunning mountains and lush forests, this freshwater lake provides a refreshing escape for swimmers.
-      </p>
-      <h3 className='country-place'>2. Inch Beach, County Kerry</h3>
-      <p className='country-desc'>Situated on the Dingle Peninsula, Inch Beach is a long, sandy stretch that provides an ideal location for wild swimming. With its Atlantic waves and scenic surroundings, this Blue Flag beach offers a unique swimming experience.
-      </p>
-      <h3 className='country-place'>3. Lough Gill, County Sligo</h3>
-      <p className='country-desc'>Lough Gill, located near Sligo town, is a picturesque freshwater lake offering numerous swimming opportunities. Surrounded by scenic woodlands and mountains, this peaceful lake provides a serene setting for a wild swim.
-      </p>
-      <h3 className='country-place'>4. Lough Hyne, County Cork</h3>
-      <p className='country-desc'>Located near Skibbereen in County Cork, Lough Hyne is a saltwater lake renowned for its exceptional beauty and calm waters. This marine lake provides a tranquil setting for swimmers, surrounded by lush hills and stunning scenery.
-      </p>
-      <h3 className='country-place'>5. Salthill Quarry, County Galway</h3>
-      <p className='country-desc'>Located near the coastal city of Galway, Salthill Quarry is a hidden gem for wild swimming. The crystal-clear waters of this former quarry create a serene swimming spot, often attracting locals and visitors seeking a refreshing dip.
-      </p>
-      <h3 className='country-place'>6. Silver Strand, County Donegal</h3>
-      <p className='country-desc'>Tucked away near Malin Beg in County Donegal, Silver Strand is a secluded beach that is perfect for wild swimming. Its pristine waters and picturesque surroundings, including dramatic cliffs, make it a truly magical spot.      </p>
-      <h3 className='country-place'>7. The Forty Foot, County Dublin</h3>
-      <p className='country-desc'>Situated in Sandycove, County Dublin, the Forty Foot is a popular spot for wild swimming and has a rich history. This iconic bathing area offers deep waters, tidal pools, and a diving platform, attracting swimmers of all ages and abilities.
-      </p>
-    </>
+                </p>
+                <p>Be mindful of any local regulations or restrictions that may apply to specific areas. Some water bodies or beaches may have specific bylaws, designated swimming areas, or seasonal restrictions. Check with local authorities or signs for any specific rules.</p>
+            </div>
 
+            <div className="article-container">
+                <div className="article-img">
+                    <img src={img1} alt="Glendalough Upper Lake, County Wicklow"/>
+                </div>
+                <div className="article-text">
+                    <h4>Glendalough Upper Lake, County Wicklow</h4>
+                    <p>
+                    Nestled within the picturesque Glendalough Valley, the Upper Lake in County Wicklow offers an enchanting setting for wild swimming. Surrounded by stunning mountains and lush forests, this freshwater lake provides a refreshing escape for swimmers.
+                    </p>
+                </div>
+            </div>
+
+            <div className="article-container article-container-reverse">
+                <div className="article-img">
+                    <img src={img2} alt="Inch Beach, County Kerry"/>
+                </div>
+                <div className="article-text">
+                    <h4>Inch Beach, County Kerry</h4>
+                    <p>
+                    Situated on the Dingle Peninsula, Inch Beach is a long, sandy stretch that provides an ideal location for wild swimming. With its Atlantic waves and scenic surroundings, this Blue Flag beach offers a unique swimming experience.
+                    </p>
+                </div>
+            </div>
+
+            <div className="article-container">
+                <div className="article-img">
+                    <img src={img3} alt="Lough Gill, County Sligo"/>
+                </div>
+                <div className="article-text">
+                    <h4>Lough Gill, County Sligo</h4>
+                    <p>
+                    Lough Gill, located near Sligo town, is a picturesque freshwater lake offering numerous swimming opportunities. Surrounded by scenic woodlands and mountains, this peaceful lake provides a serene setting for a wild swim.
+                    </p>
+                </div>
+            </div>
+
+            <div className="article-container article-container-reverse">
+                <div className="article-img">
+                    <img src={img4} alt="Lough Hyne, County Cork"/>
+                </div>
+                <div className="article-text">
+                    <h4>Lough Hyne, County Cork</h4>
+                    <p>
+                    Located near Skibbereen in County Cork, Lough Hyne is a saltwater lake renowned for its exceptional beauty and calm waters. This marine lake provides a tranquil setting for swimmers, surrounded by lush hills and stunning scenery.
+                    </p>
+                </div>
+            </div>
+
+            <div className="article-container">
+                <div className="article-img">
+                    <img src={img5} alt="Salthill Quarry, County Galway"/>
+                </div>
+                <div className="article-text">
+                    <h4>Salthill Quarry, County Galway</h4>
+                    <p>
+                    Located near the coastal city of Galway, Salthill Quarry is a hidden gem for wild swimming. The crystal-clear waters of this former quarry create a serene swimming spot, often attracting locals and visitors seeking a refreshing dip.
+                    </p>
+                </div>
+            </div>
+
+            <div className="article-container article-container-reverse">
+                <div className="article-img">
+                    <img src={img6} alt="Silver Strand, County Donegal"/>
+                </div>
+                <div className="article-text">
+                    <h4>Silver Strand, County Donegal</h4>
+                    <p>
+                    Tucked away near Malin Beg in County Donegal, Silver Strand is a secluded beach that is perfect for wild swimming. Its pristine waters and picturesque surroundings, including dramatic cliffs, make it a truly magical spot.
+                    </p>
+                </div>
+            </div>
+          </div>
   );
 };
 
